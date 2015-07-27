5d0565913cd1cd21b51002249f1ac1902030bfbb
Shanna M. Sullivan <shannasullivanold@Shannas-MacBook-Pro.local>
2015-07-26 21:18:24 -0700

    initial commit. toDoList doesn't show up in html yet.

---
+++ toDoList.js
@@ -0,0 +1,66 @@
+//javaScript for toDoListProject
+
+// VERSION 1 ------------ DOESN'T APPEND HTML
+
+//create empty array to hold toDoList items
+var toDoList = [];
+var toDoListLength = toDoList.length;
+
+
+//function to insert toDoList array into HTML
+function appendItem() {
+	for(i = 0; i < toDoListLength; i++) {
+		//create new li tag
+		var newLine = document.createElement('li');
+		//add toDoListItem text to new p tag
+		newLine.innerHTML = toDoList[i];
+		//append p tag with text to the list
+		document.getElementById('list').appendChild(newLine);
+	};
+};
+
+//after window loads, run scripts
+window.onload = function() {
+	//prompt user for new toDoListItem
+	document.getElementById('inputButton').onclick = function() {
+		var toDoListItem = prompt('What do you want to add?');
+		//add new toDoListItem to array
+		toDoList.push(toDoListItem);
+		appendItem();
+	};
+	
+};
+
+/* VERSION 2 ----------- ALSO DOESN'T APPEND HTML
+
+var itemPlusHTML = "";
+
+//function to insert toDoList array into HTML
+function appendItem() {
+	for(i = 0; i < toDoListLength; i++) {
+		//variable for each toDoList item
+		var newLine = toDoList[i];
+		//add html to new toDoList item
+		itemPlusHTML += '<li>' + newLine + '<li>';
+	};
+	document.getElementById('list').innerHTML = itemPlusHTML;
+};
+
+//after window loads, run scripts
+window.onload = function() {
+	//prompt user for new toDoListItem
+	document.getElementById('inputButton').onclick = function() {
+		var toDoListItem = prompt('What do you want to add?');
+		//add new toDoListItem to array
+		toDoList.push(toDoListItem);
+		appendItem();
+	};
+};
+
+*/
+
+
+
+
+
+
