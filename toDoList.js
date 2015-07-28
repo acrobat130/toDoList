1a325ffda1bc1e4800dc2cb08c93969046f1e119
Shanna M. Sullivan <shannasullivanold@Shannas-MacBook-Pro.local>
2015-07-27 22:42:29 -0700

    turned-in version 1. remove button hidden because not functional yet.

--- toDoList.js
+++ toDoList.js
@@ -1,76 +1,33 @@
 //javaScript for toDoListProject
 
-// VERSION 1 ------------ DOESN'T APPEND HTML
-
 //create empty array to hold toDoList items
 var toDoList = [];
 
-//function to insert toDoList array into HTML
-
-/*
-
-function appendItem() {
-	for(var item in toDoList) {
-		
-		//create new li tag
-		var newLine = document.createElement('li');
-		//add toDoListItem text to new p tag
-		newLine.innerHTML = toDoList[item];
-		//append p tag with text to the list
-		document.getElementById('list').appendChild(newLine);
-	};
-};
-
-*/
+//function to insert toDoList array items into HTML
 function appendItem(item) {
-		//create new li tag
-			var newLine = document.createElement('li');
-			//add toDoListItem text to new p tag
-			newLine.innerHTML = item;
-			//append p tag with text to the list
-			document.getElementById('list').appendChild(newLine);
+	//create new li tag
+	var newLine = document.createElement('li');
+	//add toDoListItem text (and future removeButton) to new p tag
+	newLine.innerHTML = item/* + ' ' + '<button id="removeButton">Remove</button>'*/;
+	//append p tag with text to the list
+	document.getElementById('list').appendChild(newLine);
 };
 
 //after window loads, run scripts
 window.onload = function() {
 	//prompt user for new toDoListItem
 	document.getElementById('inputButton').onclick = function() {
+		//save prompt answer to variable
 		var toDoListItem = prompt('What do you want to add?');
 		//add new toDoListItem to array
 		toDoList.push(toDoListItem);
+		//add new toDoListItem to HTML
 		appendItem(toDoListItem);
 	};
+	//future - function to remove toDoList items
+	//document.getElementById('removeButton').onclick = function(){};
 	
 };
-
-/* VERSION 2 ----------- ALSO DOESN'T APPEND HTML
-
-var itemPlusHTML = "";
-
-//function to insert toDoList array into HTML
-function appendItem() {
-	for(i = 0; i < toDoListLength; i++) {
-		//variable for each toDoList item
-		var newLine = toDoList[i];
-		//add html to new toDoList item
-		itemPlusHTML += '<li>' + newLine + '<li>';
-	};
-	document.getElementById('list').innerHTML = itemPlusHTML;
-};
-
-//after window loads, run scripts
-window.onload = function() {
-	//prompt user for new toDoListItem
-	document.getElementById('inputButton').onclick = function() {
-		var toDoListItem = prompt('What do you want to add?');
-		//add new toDoListItem to array
-		toDoList.push(toDoListItem);
-		appendItem();
-	};
-};
-
-*/
-
 
 
 
